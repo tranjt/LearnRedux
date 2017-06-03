f66b7cccb506955f24938f7741d221484e7629a7
JTTran <tranjt80@gmail.com>
2017-06-03 17:09:20 +0300

    Break up reducer into multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -10,7 +10,7 @@ var stateDefault = {
 
 var nextHobbyId = 1;
 var nextMovieId = 1;
-var reducer = (state = stateDefault, action)=> {
+var oldReducer = (state = stateDefault, action)=> {
     //state = state || {name: "Anonymous"};
     
     switch(action.type) {
@@ -56,6 +56,57 @@ var reducer = (state = stateDefault, action)=> {
     }    
 };
 
+var nameReducer = (state = "Anonymous", action)=> {
+    switch(action.type) {
+        case "CHANGE_NAME":
+            return action.name
+        default: 
+            return state;
+    };
+};
+
+var hobbiesReducer = (state = [], action)=> {
+    switch(action.type) {
+        case "ADD_HOBBY":
+            return [
+                ...state, 
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }     
+            ];
+        case "REMOVE_HOBBY":
+            return state.filter((hobby)=> hobby.id !== action.id);
+        default: 
+            return state;
+    };
+};
+
+var moviesReducer = (state = [], action)=> {
+    switch(action.type) {
+        case "ADD_MOVIE":
+            return [
+                ...state,
+                {
+                    id: nextMovieId++,
+                    title: action.title,
+                    genre: action.genre
+                }
+            ];
+        case "REMOVE_MOVIE":
+            return state.filter((movie)=> movie.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+var reducer = redux.combineReducers({
+    name: nameReducer,
+    hobbies: hobbiesReducer,
+    movies: moviesReducer
+   
+});
+
 var store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension(): f => f
 ));
@@ -98,8 +149,6 @@ store.dispatch({
     type:"REMOVE_HOBBY",
     id:2
 })
-
-
 
 store.dispatch({
     type: "CHANGE_NAME",
