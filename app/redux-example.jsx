1c8056f0304f74e6720bf9508ea01173e7abb859
JTTran <tranjt80@gmail.com>
2017-06-03 19:12:36 +0300

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require("redux");
+var axios = require("axios");
 
 console.log("starting redux example");
 
@@ -94,10 +95,59 @@ var removeMovie = (id)=> {
     };
 };
 
+/*  
+    Map reducer and action generators
+*/
+
+var mapReducer = (state = {isFetching: false, url: undefined}, action )=> {
+    switch(action.type) {
+        case "START_LOCATION_FETCH":
+            return {
+                isFetching: true,
+                url: undefined
+
+            };
+        case "COMPLETE_LOCATION_FETCH":
+            return {
+                isFetching: false,
+                url: action.url
+            };
+        default:
+            return state;
+    }
+};
+
+var startLocationFetch = ()=> {
+    return {
+        type: "START_LOCATION_FETCH"
+    }
+};
+
+var completeLocationFetch = (url)=> {
+    return {
+        type: "COMPLETE_LOCATION_FETCH",
+        url
+    }
+};  
+
+var fetchLocation = ()=> {
+    store.dispatch(startLocationFetch());
+    axios.get("http://ipinfo.io").then(function(res) {        
+        var loc = res.data.loc;
+        var baseUrl = "http://maps.google.com?q=";
+
+        store.dispatch(completeLocationFetch(baseUrl + loc));
+    });
+};
+
+
+
+
 var reducer = redux.combineReducers({
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map: mapReducer
    
 });
 
@@ -108,17 +158,23 @@ var store = redux.createStore(reducer, redux.compose(
 
 // Subscribe to changes
 var unsubscribe = store.subscribe(()=>{
-    var state = store.getState();
-    console.log("Name is ", state.name);
-    document.getElementById("app").innerHTML = state.name;
+    var state = store.getState();    
 
     console.log("New state ", store.getState());
+
+    if (state.map.isFetching) {
+        document.getElementById("app").innerHTML = "Loading...";
+    } else if (state.map.url) {
+        document.getElementById("app").innerHTML = `<a target="__blank" href="${state.map.url}">View your location</a>`;
+    }
 });
 // unsubscribe();
 
 var currentState = store.getState();
 
 console.log("currentState ", currentState);
+
+fetchLocation();
 
 
 store.dispatch(changeName("Joe"));
