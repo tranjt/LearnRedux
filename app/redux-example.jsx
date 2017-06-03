3fe2b415464e0fad2849f213247030033e3ef8c5
JTTran <tranjt80@gmail.com>
2017-06-03 19:41:38 +0300

    Refactor file to create seprate file for action, reducer and store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,160 +1,11 @@
 var redux = require("redux");
-var axios = require("axios");
+
 
 console.log("starting redux example");
 
-/*
-    Name reducer and action generators
-*/
-var nameReducer = (state = "Anonymous", action)=> {
-    switch(action.type) {
-        case "CHANGE_NAME":
-            return action.name
-        default: 
-            return state;
-    };
-};
-
-var changeName = (name)=> {
-    return {
-        type: "CHANGE_NAME",
-        name
-    };
-};
-
-/*
-    Hobby reducer and action generators
-*/ 
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action)=> {
-    switch(action.type) {
-        case "ADD_HOBBY":
-            return [
-                ...state, 
-                    {
-                        id: nextHobbyId++,
-                        hobby: action.hobby
-                    }     
-            ];
-        case "REMOVE_HOBBY":
-            return state.filter((hobby)=> hobby.id !== action.id);
-        default: 
-            return state;
-    };
-};
-
-var addHobby = (hobby)=> {
-    return {
-        type: "ADD_HOBBY",
-        hobby
-    };
-};
-
-var removeHobby = (id)=> {
-    return {
-        type: "REMOVE_HOBBY",
-        id
-    };
-};
-
-
-/*
-    Movie reducer and action generators
-*/ 
-var nextMovieId = 1;
-var moviesReducer = (state = [], action)=> {
-    switch(action.type) {
-        case "ADD_MOVIE":
-            return [
-                ...state,
-                {
-                    id: nextMovieId++,
-                    title: action.title,
-                    genre: action.genre
-                }
-            ];
-        case "REMOVE_MOVIE":
-            return state.filter((movie)=> movie.id !== action.id);
-        default:
-            return state;
-    }
-};
-
-var addMovie = (movie, genre)=> {
-    return {
-        type: "ADD_MOVIE",
-        movie,
-        genre
-    };
-};
-
-var removeMovie = (id)=> {
-    return {
-        type: "REMOVE_MOVIE",
-        id
-    };
-};
-
-/*  
-    Map reducer and action generators
-*/
-
-var mapReducer = (state = {isFetching: false, url: undefined}, action )=> {
-    switch(action.type) {
-        case "START_LOCATION_FETCH":
-            return {
-                isFetching: true,
-                url: undefined
-
-            };
-        case "COMPLETE_LOCATION_FETCH":
-            return {
-                isFetching: false,
-                url: action.url
-            };
-        default:
-            return state;
-    }
-};
-
-var startLocationFetch = ()=> {
-    return {
-        type: "START_LOCATION_FETCH"
-    }
-};
-
-var completeLocationFetch = (url)=> {
-    return {
-        type: "COMPLETE_LOCATION_FETCH",
-        url
-    }
-};  
-
-var fetchLocation = ()=> {
-    store.dispatch(startLocationFetch());
-    axios.get("http://ipinfo.io").then(function(res) {        
-        var loc = res.data.loc;
-        var baseUrl = "http://maps.google.com?q=";
-
-        store.dispatch(completeLocationFetch(baseUrl + loc));
-    });
-};
-
-
-
-
-var reducer = redux.combineReducers({
-    name: nameReducer,
-    hobbies: hobbiesReducer,
-    movies: moviesReducer,
-    map: mapReducer
-   
-});
-
-var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension(): f => f
-));
-
+var actions = require("./actions/index");
+var store = require("./store/configureStore").configure();
+console.log(store);
 
 // Subscribe to changes
 var unsubscribe = store.subscribe(()=>{
@@ -169,26 +20,26 @@ var unsubscribe = store.subscribe(()=>{
     }
 });
 // unsubscribe();
-
+ 
 var currentState = store.getState();
 
 console.log("currentState ", currentState);
 
-fetchLocation();
+store.dispatch(actions.fetchLocation());
 
 
-store.dispatch(changeName("Joe"));
+store.dispatch(actions.changeName("Joe"));
 
 
-store.dispatch(addHobby("Running"));
-store.dispatch(addHobby("Eating"));
-store.dispatch(removeHobby(2));
+store.dispatch(actions.addHobby("Running"));
+store.dispatch(actions.addHobby("Eating"));
+store.dispatch(actions.removeHobby(2));
 
-store.dispatch(changeName("Emily"));
+store.dispatch(actions.changeName("Emily"));
 
-store.dispatch(addMovie("Cookie eater", "Action"));
-store.dispatch(addMovie("Batman", "Comedy"));
-store.dispatch(removeMovie(2));
+store.dispatch(actions.addMovie("Cookie eater", "Action"));
+store.dispatch(actions.addMovie("Batman", "Comedy"));
+store.dispatch(actions.removeMovie(2));
 
 
 
