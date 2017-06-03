830aee73c778f7c8f8d6b7a29832efbf13bc0bad
JTTran <tranjt80@gmail.com>
2017-06-03 14:01:13 +0300

    Add support for CHANGE_NAME and SEARCH_TEXT action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,10 +4,29 @@ console.log("starting redux example");
 
 var reducer = (state = {name: "Anonymous"}, action)=> {
     //state = state || {name: "Anonymous"};
-    return state;
+    
+    switch(action.type) {
+        case "CHANGE_NAME":
+            return {
+                ...state,
+                name: action.name
+            };
+        default:
+            return state;
+    }    
 };
+
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 
-console.log("currentState", currentState);
+console.log("currentState ", currentState);
+
+var action = {
+    type: "CHANGE_NAME",
+    name: "Joe"
+};
+
+store.dispatch(action);
+
+console.log("Name should be Joe", store.getState());
--- app/redux-todo-examples.jsx
+++ app/redux-todo-examples.jsx
@@ -8,10 +8,27 @@ var stateDefault =  {
 }
 
 var reducer = (state = stateDefault, action) => {
-    return state;
+
+    switch(action.type) {
+        case "CHANGE_SEARCH_TEXT":
+            return {
+                ...state,
+                searchText: action.searchText
+            };
+        default:
+            return state;
+    }    
 };
 
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log("currentState", currentState);
+
+var action = {
+    type: "CHANGE_SEARCH_TEXT",
+    searchText: "Cookie"
+};
+
+store.dispatch(action);
+console.log("New searchText should be Cookie",store.getState());
